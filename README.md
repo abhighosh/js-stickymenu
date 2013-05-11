9d8a3dace3ff4e0dd08cbb6ba73ab2e305c14d34
Abhi Ghosh <agonline@gmail.com>
2013-05-11 18:50:05 -0400

    Version 1.0.0
    First commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# [Stickymenu.js](http://demos.abhighosh.co.uk/stickymenu)
+
+Stickymenu.js is a lightweight jQuery plugin for neatly and smoothly keeping menus and other ui elements visible at the top of a page.
+
+* Source: [https://github.com/h5bp/html5-boilerplate](https://github.com/h5bp/html5-boilerplate)
+* Homepage: [http://demos.abhighosh.co.uk/stickymenu](http://demos.abhighosh.co.uk/stickymenu)
+
+
+## Quick start
+
+1. Download latest version of stickymenu.js
+
+2. Include jQuery
+
+```javascript
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+ ```
+
+3. Include stickymenu.js with a path to the containing folder
+
+```javascript
+<script src="stickymenu-1.0.0.js"></script>
+ ```
+
+4. Initialise sticky menu for an element.
+
+```javascript
+<script>
+$(document).ready(function(){
+   $("#stickymenu").stickymenu();
+  });
+  </script>
+ ```
+
+
+## Features
+
+* Touch friendly
+* Cross browser compatible (IE6+, Chrome, Safari, Firefox)
---
+++ js/stickymenu-1.0.0.js
@@ -0,0 +1,167 @@
+/*
+* Author: Abhi Ghosh (abhighosh)
+* Created: 5/11/2013
+* Date: 5/11/2013
+* Website: http://www.abhighosh.co.uk/stickymenu
+* Description: Sticks elements smoothly to the top of a page, animating on touch devices as necessary
+ */
+
+;(function ( $, window, document, undefined ) {
+    
+    var pluginName = 'stickymenu',
+        defaults = {
+'offset': 0,
+'neverAnimate': false,
+'touchDisable': false
+        };
+
+    function stickymenu( element, options ) {
+        this.element = element;
+		
+        this.options = $.extend( {}, defaults, options) ;
+        
+        this._defaults = defaults;
+        this._name = pluginName;
+		
+		var offsetFromTop = this.options.offset;
+		
+//If not IE6 or older browser and not touch device
+if (((typeof document.body.style.maxHeight != "undefined") && !('ontouchstart' in document)) || (this.options.neverAnimate == true)) {
+	//If IE7 or newer or other modern browser, position:fixed is supported so disable animations; menu will stick as required without this.
+    var animationEnabled = false;
+	var disable = false;
+	//If IE6 or older with the user option never animate set to true disable the effect altogether
+	if(typeof document.body.style.maxHeight == "undefined"){
+	disable = true;
+	}
+    } else{
+	//If IE6, enable animations as position: fixed is unsupported, and using absolute positioning alone leads to loss of smooth tracking of element. 
+	//On touch devices, the scroll event is only triggered on finishing scroll, so enable animations by default.
+    var animationEnabled = true;
+	var disable = false;
+}
+	
+	//If user has chosen to disable the effect altogether on touch
+	if('ontouchstart' in document && this.options.touchDisable == true){
+	disable = true;
+	}
+
+//Ensure the sticky menu element defined exists in the document
+  if (!!$(element).offset() && disable == false) {
+	
+	//Get the initial offset of the sticky element
+    var stickyTop = $(element).offset().top;
+	var stickyLeft = $(element).offset().left;
+	
+	//Get the height, width and float of the sticky element
+	var stickyHeight = $(element).outerHeight();
+	var stickyWidth = $(element).outerWidth();
+	var stickyFloat = $(element).css('float');
+	var stickyMargin = $(element).css('margin');
+	
+	//Create a name for our replacement div
+	var replacementDiv = "sticky-replacement-"+$(element).attr('id');
+	
+	//Insert replacement div before the element with correct replacement attributes.
+	$(element).before("<div id='"+replacementDiv+"' style='display: block; margin: "+stickyMargin+"; padding: 0; height:"+stickyHeight+"px; width:"+stickyWidth+"px; float:"+stickyFloat+"'>&nbsp</div>");
+	
+	//If the top offset is more than 0
+	if(stickyTop>0){
+	
+	//Set absolute positioning of sticky menu as per calculated values
+	 $(element).css({ 'width': stickyWidth, position: 'absolute', top: stickyTop, left: stickyLeft});
+	 
+	//Function to be called on scrolling
+    var moveElement = function () {
+	
+	//Returns the distance the user has scrolled top and left
+      var windowTop = $(window).scrollTop();
+	  var windowLeft = $(window).scrollLeft();
+	  //Check if the user has scrolled past the sticky menu element threshold
+      if (stickyTop < (windowTop+offsetFromTop)){
+		 
+		  //If animation is disabled
+	  if(animationEnabled == false){
+		  
+		  //Set position to fixed. Set left value for horizontal scrolling support if required.
+		  $(element).css({ position: 'fixed', top: offsetFromTop, left:  stickyLeft-windowLeft});
+		  } else {
+		  
+		  //If animations are enabled, animate from current position to distance user has scrolled
+		  $(element).animate({top: windowTop+offsetFromTop}, 200);
+		  }
+	}
+	  
+	  //If user has scrolled back up above the sticky menu element
+      else {
+		  
+		  //If animation is disabled
+		  if(animationEnabled == false){
+			  
+			  //Set position to absolute, setting left value for horizontal scrolling support if required.
+		  $(element).css({ position: 'absolute', top: stickyTop, left:  stickyLeft});
+		  } else {
+	
+	//Animate from current position to distance user has scrolled
+	$(element).animate({top: stickyTop}, 200);
+		  }
+      }
+    };
+	
+	//Define timer to delay animation
+	var timer;
+	
+	//On window scroll
+    $(window).bind('scroll',function () {
+		
+		//If animations are enabled, set the delay for calling our scroll function
+		if(animationEnabled == true){
+        clearTimeout(timer);
+        timer = setTimeout( moveElement , 50 );
+		} else {
+		
+		//If animations are disabled, call scroll function on each scroll event
+			moveElement();
+		}
+    });
+	
+	//If window is resized, get the left offset of the replacement div to calculate the required left offset of the sticky element
+	$(window).resize(function() {
+		stickyLeft = $("#"+replacementDiv).offset().left;
+		$(element).css({left:  stickyLeft});
+  
+});
+ 
+//Load the move menu function on page load
+if('ontouchstart' in document){
+	$(window).scrollTop(0);
+	} else {
+		moveElement();
+	}
+
+} else {
+	//If the top offset is 0, set the width and height of the element appropriately and set it to fixed
+	var stickyHeightRaw = $(element).height();
+	var stickyWidthRaw = $(element).width();
+	$(element).css({position: 'fixed', top: 0, height: stickyHeightRaw, width: stickyWidthRaw});
+}	
+	}
+        
+        this.init();
+    }
+
+    stickymenu.prototype.init = function () {
+
+
+    };
+
+    $.fn[pluginName] = function ( options ) {
+        return this.each(function () {
+            if (!$.data(this, 'plugin_' + pluginName)) {
+                $.data(this, 'plugin_' + pluginName, 
+                new stickymenu( this, options ));
+            }
+        });
+    }
+
+})( jQuery, window, document );
